afadb56dc750f8ee89d0777e967e55c9bde5c489
AnjaP <treyjapan@pm.me>
2022-05-11 22:06:56 +0200

    adds account, login and signup links

--- src/App.js
+++ src/App.js
@@ -1,13 +1,21 @@
 import { Route, Routes } from 'react-router-dom';
 import Navbar from './components/Navbar';
+import { AuthContextProvider } from './context/AuthContext';
 import Home from './pages/Home';
+import Login from './pages/Login';
+import Signup from './pages/Signup';
+import Account from './pages/Account';
 
 function App() {
 	return (
 		<>
+			<AuthContextProvider />
 			<Navbar />
 			<Routes>
 				<Route path="/" element={<Home />} />
+				<Route path="/login" element={<Login />} />
+				<Route path="/signup" element={<Signup />} />
+				<Route path="/account" element={<Account />} />
 			</Routes>
 		</>
 	);
