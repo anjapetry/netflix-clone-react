d4a21ad2ffbca12bb1e72880bfcee0faa8050cca
AnjaP <treyjapan@pm.me>
2022-05-11 14:51:19 +0200

    adds slider and icons

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -7,11 +7,15 @@ const Home = () => {
 	return (
 		<>
 			<Main />
-			<Row title="Now Playing" fetchURL={requests.requestNowPlaying} />
-			<Row title="Upcoming" fetchURL={requests.requestUpcoming} />
-			<Row title="Popular" fetchURL={requests.requestPopular} />
-			<Row title="Trending" fetchURL={requests.requestTrending} />
-			<Row title="Top Rated" fetchURL={requests.requestTopRated} />
+			<Row
+				rowId="1"
+				title="Now Playing"
+				fetchURL={requests.requestNowPlaying}
+			/>
+			<Row rowID="2" title="Upcoming" fetchURL={requests.requestUpcoming} />
+			<Row rowID="3" title="Popular" fetchURL={requests.requestPopular} />
+			<Row rowID="4" title="Trending" fetchURL={requests.requestTrending} />
+			<Row rowID="5" title="Top Rated" fetchURL={requests.requestTopRated} />
 		</>
 	);
 };
